814b1eec9fcc0662842f813c944920779269f14f
Murillo <murilloooliveira12@gmail.com>
2025-09-18 13:45:12 -0300

    configurar conecxao com o banco de dados

---
+++ src/main/java/br/com/murillo/salutar/SalutarApplication.java
@@ -0,0 +1,13 @@
+package br.com.murillo.salutar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalutarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalutarApplication.class, args);
+	}
+
+}
